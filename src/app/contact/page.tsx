87a3671251f376e18c4ad72f4508bc6a24266581
RIO-T-PRO <malibaalex42@gmail.com>
2025-11-01 17:02:54 +0300

    Just finish the contact before the responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="flex flex-col max-w-5xl mx-auto space-y-10 my-10">
+      <h2 className="text-3xl text-center font-bold text-black ">CONTACT US</h2>
+
+      <div className="flex justify-center items-center">
+        <form action="" className="flex flex-col w-3/5 space-y-12">
+          <input
+            type="text"
+            placeholder="Name"
+            className="text-xl border-b-2 border-b-gray-800 w-full outline-none"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="text-xl border-b-2 border-b-gray-800 w-full outline-none"
+          />
+          <input
+            type="text"
+            placeholder="Telephone"
+            className="text-xl border-b-2 border-b-gray-800 w-full outline-none"
+          />
+          <textarea
+            name="Message"
+            placeholder="Message"
+            className="text-xl border-b-2 border-b-gray-800 w-full h-full outline-none"
+          ></textarea>
+        </form>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <button className="text-center px-8 text-xl py-2 rounded-2xl bg-rose-600 text-white">
+          Send
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ import { FaSearch, FaShoppingCart } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="flex justify-between items-center p-3 sticky top-0 bg-white/10 backdrop-blur-2xl mb-4">
-      <h3 className="font-bold text-4xl">Selling</h3>
+      <h2 className="font-bold text-4xl">Selling</h2>
       <nav className="flex">
         <FaBars className="hidden" />
         <ul className="flex gap-4 text-xl font-semibold">
